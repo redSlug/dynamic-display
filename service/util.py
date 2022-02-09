06f7c3f29811f2328eaa41cffd52f7884551fb4b
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2022-02-08 19:09:47 -0500

    Revert dotenv path in util.py

--- service/util.py
+++ service/util.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 
-DOTENV_PATH = "/app/hostenv/dotenv"
+DOTENV_PATH = "/app/env/dotenv"
 
 load_dotenv(find_dotenv(DOTENV_PATH))
 DARK_SKY_API_KEY = os.getenv("DARK_SKY_API_KEY")
