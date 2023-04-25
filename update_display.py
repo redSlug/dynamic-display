0f754f036822efd730628716c07357ece80c65e6
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2023-04-25 19:47:51 -0400

    remove dark sky

--- update_display.py
+++ update_display.py
@@ -4,8 +4,7 @@ from service.banner_maker import BannerMaker
 from service.calendar import write_calendar_data, get_calendar_text
 from service.messages import get_recent_message
 from service.util import special_logger
-from service.weather import DarkSkyWeather
-
+    
 from service.util import DOTENV_PATH
 
 
@@ -17,21 +16,11 @@ if __name__ == "__main__":
     except Exception as e:
         special_logger(f"Could not get calendar data exception={e}")
 
-    try:
-        dsw = DarkSkyWeather()
-        weather = dsw.get_weather()
-    except Exception as e:
-        special_logger(f"Could not get weather data exception={e}")
-        raise
-
     message = get_recent_message()
 
     special_logger(f"message={message}")
 
     rc_banner = BannerMaker(banner_id="")
     rc_banner.replace_banner(
-        weather=weather or " ", calendar=calendar or " ", message=message
+        weather=" ", calendar=calendar or " ", message=message
     )
-
-    banner = BannerMaker(banner_id="_2")
-    banner.replace_banner(weather=weather)
